16af1b8e39fa275d45671be550c5207e3fd3c69d
renanls <142767050+renanls6@users.noreply.github.com>
2025-04-04 13:15:05 -0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,155 +1,154 @@
-echo "   ██████╗ ██╗  ██╗    ██████╗ ███████╗███╗   ██╗ █████╗ ███╗   ██╗"
-echo " ██╔═████╗╚██╗██╔╝    ██╔══██╗██╔════╝████╗  ██║██╔══██╗████╗  ██║"
-echo " ██║██╔██║ ╚███╔╝     ██████╔╝█████╗  ██╔██╗ ██║███████║██╔██╗ ██║"
-echo "████╔╝██║ ██╔██╗     ██╔══██╗██╔══╝  ██║╚██╗██║██╔══██║██║╚██╗██║ "
-echo "╚██████╔╝██╔╝ ██╗    ██║  ██║███████╗██║ ╚████║██║  ██║██║ ╚████║"
-echo " ╚═════╝ ╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═══╝"
+echo "   ______ _____   ___  ____  __________  __ ____  _____ "
+echo "  / __/ // / _ | / _ \/ __/ /  _/_  __/ / // / / / / _ )"
+echo " _\ \/ _  / __ |/ , _/ _/  _/ /  / /   / _  / /_/ / _  |"
+echo "/___/_//_/_/ |_/_/|_/___/ /___/ /_/   /_//_/\____/____/ "
+echo "               SUBSCRIBE MY CHANNEL                     "
 
 sleep 5
 
-echo "Removing previously existing models..."
+echo "Menghapus model yang ada sebelumnya..."
 rm -rf /root/.cache/hyperspace/models/*
 sleep 5
 
 #!/bin/bash
-echo "Enter your private key (end with CTRL+D):"
+echo "Masukkan private key Anda (akhiri dengan CTRL+D):"
 cat > .pem
 
-read -p "Enter screen name: " screen_name
+read -p "Masukkan nama screen: " screen_name
 
 if [[ -z "$screen_name" ]]; then
-    echo "Screen name cannot be empty."
+    echo "Nama screen tidak boleh kosong."
     exit 1
 fi
 
-echo "Creating a screen session with the name '$screen_name'..."
+echo "Membuat sesi screen dengan nama '$screen_name'..."
 screen -S "$screen_name" -dm
 
-echo "Running the 'aios-cli start' command within the screen session '$screen_name'..."
+echo "Menjalankan perintah 'aios-cli start' di dalam sesi screen '$screen_name'..."
 screen -S "$screen_name" -X stuff "aios-cli start\n"
 
 sleep 5
 
-echo "Exiting the screen session '$screen_name'..."
+echo "Keluar dari sesi screen '$screen_name'..."
 screen -S "$screen_name" -X detach
 sleep 5
 
 if [[ $? -eq 0 ]]; then
-    echo "Screen session '$screen_name' successfully created and running 'aios-cli start' command."
+    echo "Screen dengan nama '$screen_name' berhasil dibuat dan menjalankan perintah aios-cli start."
 else
-    echo "Failed to create screen session."
+    echo "Gagal membuat screen."
     exit 1
 fi
 
 sleep 2
 
-echo "Adding model with the 'aios-cli models add' command..."
+echo "Menambahkan model dengan perintah aios-cli models add..."
 url="https://huggingface.com/TheBloke/phi-2-GGUF/resolve/main/phi-2.Q4_K_M.gguf"
 model_folder="/root/.cache/hyperspace/models/hf__TheBloke___phi-2-GGUF__phi-2.Q4_K_M.gguf"
 model_path="$model_folder/phi-2.Q4_K_M.gguf"
 
 if [[ ! -d "$model_folder" ]]; then
-    echo "Folder not found, creating folder $model_folder..."
+    echo "Folder tidak ditemukan, membuat folder $model_folder..."
     mkdir -p "$model_folder"
 else
-    echo "Folder already exists, continuing..."
+    echo "Folder sudah ada, melanjutkan..."
 fi
 
 if [[ ! -f "$model_path" ]]; then
-    echo "Downloading model from $url..."
+    echo "Mengunduh model dari $url..."
     while true; do
         if wget -q "$url" -O "$model_path"; then
-            echo "Model successfully downloaded and saved to $model_path!"
+            echo "Model berhasil diunduh dan disimpan di $model_path!"
             break
         else
-            echo "An error occurred while downloading the model. Retrying..."
+            echo "Terjadi kesalahan saat mengunduh model. Mengulang..."
             sleep 3  
         fi
     done
 else
-    echo "Model already exists at $model_path, skipping download."
+    echo "Model sudah ada di $model_path, melewati proses pengunduhan."
 fi
 
-echo "Model successfully added!"
+echo "Model berhasil ditambahkan!"
 
-echo "Running inference using the added model..."
-read -p "Do you want to run inference? (y/n): " user_choice
+echo "Menjalankan inferensi menggunakan model yang telah ditambahkan..."
+read -p "Apakah Anda ingin menjalankan inferensi? (y/n): " user_choice
 
 if [[ "$user_choice" == "y" || "$user_choice" == "Y" ]]; then
-    echo "Running inference using the added model..."
+    echo "Menjalankan inferensi menggunakan model yang telah ditambahkan..."
     infer_prompt="Can you explain how to write an HTTP server in Rust?"
 
     while true; do
         if aios-cli infer --model hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf --prompt "$infer_prompt"; then
-            echo "Inference successful."
+            echo "Inferensi berhasil."
             break
         else
-            echo "An error occurred while running inference. Retrying..."
+            echo "Terjadi kesalahan saat menjalankan inferensi. Mengulang..."
             sleep 3
         fi
     done
 else
-    echo "Inference step skipped."
+    echo "Langkah inferensi dilewati."
 fi
 
-echo "Running import-keys command with file.pem..."
+echo "Menjalankan perintah import-keys dengan file.pem..."
 aios-cli hive import-keys ./.pem
 
 sleep 5
 
-echo "Running login and select-tier..."
+echo "Menjalankan login dan select-tier..."
 aios-cli hive login
 aios-cli hive select-tier 5
 sleep 5
 
-echo "Running Hive inference with the added model..."
-read -p "Do you want to run inference for the first model? (y/n): " user_choice
+echo "Menjalankan Hive inferensi menggunakan model yang telah ditambahkan..."
+read -p "Apakah Anda ingin menjalankan inferensi model pertama? (y/n): " user_choice
 
 if [[ "$user_choice" == "y" || "$user_choice" == "Y" ]]; then
-    echo "Running inference using the added model..."
+    echo "Menjalankan inferensi menggunakan model yang telah ditambahkan..."
     infer_prompt="how do I support the Share it hub community?"
 
     while true; do
         if aios-cli infer --model hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf --prompt "$infer_prompt"; then
-            echo "Inference successful."
+            echo "Inferensi berhasil."
             break
         else
-            echo "An error occurred while running inference. Retrying..."
+            echo "Terjadi kesalahan saat menjalankan inferensi. Mengulang..."
             sleep 3
         fi
     done
 else
-    echo "First model inference step skipped."
+    echo "Langkah inferensi model pertama dilewati."
 fi
 
-# Asking the user whether to run hive inference
-read -p "Do you want to run Hive inference? (y/n): " hive_choice
+# Menanyakan kepada pengguna apakah ingin menjalankan hive infer
+read -p "Apakah Anda ingin menjalankan inferensi Hive? (y/n): " hive_choice
 
 if [[ "$hive_choice" == "y" || "$hive_choice" == "Y" ]]; then
-    # Running Hive inference using the added model
-    echo "Running Hive inference using the added model..."
+    # Menjalankan inferensi Hive menggunakan model yang telah ditambahkan
+    echo "Menjalankan Hive inferensi menggunakan model yang telah ditambahkan..."
     infer_prompt="how do I support the Share it hub community?"
 
     while true; do
         if aios-cli hive infer --model "$model" --prompt "$infer_prompt"; then
-            echo "Hive inference successful."
+            echo "Hive Inferensi berhasil."
             break
         else
-            echo "An error occurred while running Hive inference. Retrying..."
+            echo "Terjadi kesalahan saat menjalankan inferensi. Mengulang..."
             sleep 3
         fi
     done
 else
-    echo "Hive inference step skipped."
+    echo "Langkah Hive inferensi dilewati."
 fi
 
 sleep 5
 
-echo "Stopping the 'aios-cli start' process with 'aios-cli kill'..."
+echo "Menghentikan proses 'aios-cli start' dengan 'aios-cli kill'..."
 aios-cli hive login
 sleep 5
 
 aios-cli hive connect
 sleep 5
 
-echo "DONE. IF YOU WANT TO CHECK USE THE COMMAND : screen -r ""name of screen created without quotes "" !"
+echo "DONE. JIKA KALIAN INGIN CHECK GUNAKAN PERINTAH : sceen -r ""nama screen yang dibuat tanpa tanda " """ !"
